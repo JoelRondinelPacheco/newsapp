bc1d3730f2852381d54de5af3edf65ae5267e5bd
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-11-29 01:58:10 -0300

    Cambios

--- src/main/resources/static/scripts/admin_dashboard/main_search.js
+++ src/main/resources/static/scripts/admin_dashboard/main_search.js
@@ -1,1 +1,8 @@
-console.log("cargo")
+const d = document
+
+const $searchBtn = d.getElementById("searchBtn")
+const $clearSearchBtn = d.getElementById("clearSearchBtn")
+
+//fetch con los datos
+
+// Implementar busqueda con los datos: Controller, service, repository
