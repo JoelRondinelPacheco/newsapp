04d69069e6dd765f4fde65a34298a28f93d4176a
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-12-01 08:04:37 -0300

    Metodo servicio de cambiar role

--- src/main/java/com/joel/newsapp/services/interfaces/IUserRolesService.java
+++ src/main/java/com/joel/newsapp/services/interfaces/IUserRolesService.java
@@ -4,8 +4,5 @@ import com.joel.newsapp.exceptions.NotFoundException;
 import com.joel.newsapp.utils.Role;
 
 public interface IUserRolesService {
-    String changeReporterRole(String userId, Role newRole) throws NotFoundException;
-    String changeUserRole(String userId, Role newRole) throws NotFoundException;
-    String changeAdminRole(String userId, Role newRole) throws NotFoundException;
-    String changeModeratorRole(String userId, Role newRole) throws NotFoundException;
+    String changeRole(String userId, Role newRole) throws NotFoundException;
 }
