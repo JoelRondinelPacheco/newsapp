2fefd4062f8738471ac740e8e969b700ee9819d9
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-12-02 18:42:56 -0300

    Modal edicion de usuario

--- src/main/resources/static/scripts/changerole.js
+++ src/main/resources/static/scripts/changerole.js
@@ -1,28 +1,45 @@
 const d = document
 const $modalBody = d.getElementById('modal-body')
-const $options = d.querySelectorAll('.btn-modal')
-const $name = d.getElementById('modal-name-email')
-const $role = d.getElementById('modal-role')
+const $editBtn = d.querySelectorAll('.btn-modal')
+const $name = d.getElementById('moda-user-name')
+const $email = d.getElementById('modal-user-email')
+const $role = d.getElementById('modal-current-rol')
 const $closeBtn = d.getElementById('btn-modal-close')
 const $rolesList = d.getElementById('roles-list')
 const $roleDefault = d.getElementById('role-default')
 
+const rolesUrl = "http://localhost:8080/utils/roles"
+var roles;
 
-for (let i = 0; i < $options.length; i++) {
-    $options[i].addEventListener("click", function (e) {
-        $name.innerText = e.currentTarget.getAttribute('data-name')
-        $role.innerText = `${e.currentTarget.getAttribute('data-role')} ->`
-        $rolesList.innerHTML = `<select name="roles">
-        <option value="empty" id="role-default" selected>Nuevo Rol</option>
-        <option class="modal-open" value="Usuario">Cliente</option>
-        <option class="modal-open" value="Periodista">Periodista</option>
-        <option class="modal-open" value="Moderador">Moderador</option>
-        <option class="modal-open" value="Adminstrador">Adminstrador</option>
-    </select>`
+const toPascalCase = str =>
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())
+    .join('');
 
-    
-    })
+
+async function roles (url) {
+    try {
+    const res = await fetch(url)
+    const json = await res.json();
+    console.log(json)
+    roles = json
+    } catch (err) {
+        console.log("Error: " + err);
+    }
 }
+
+roles(rolesUrl)
+
+$editBtn.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        $name.innerHTML = e.currentTarget.getAttribute('data-name')
+        $email.innerHTML = e.currentTarget.getAttribute('data-email')
+        $role.innerHTML = toPascalCase(e.currentTarget.getAttribute('data-role'))
+
+    })
+})
+
 
 $closeBtn.addEventListener("click", function() {
     $name.innerText = ''
