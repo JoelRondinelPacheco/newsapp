2e358a06a579465c3609843aec44ae84976d5053
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-12-11 17:32:04 -0300

    Botones de paginas se resaltan en la actual

--- src/main/java/com/joel/newsapp/controllers/AdminDashboardController.java
+++ src/main/java/com/joel/newsapp/controllers/AdminDashboardController.java
@@ -44,6 +44,7 @@ public class AdminDashboardController {
         if (page_number == null) {
             page_number = 1;
         }
+        model.addAttribute("page", "users");
         model.addAttribute("role", role);
         model.addAttribute("state", state);
         if (role.equalsIgnoreCase(Role.USER.name())) {
@@ -81,6 +82,7 @@ public class AdminDashboardController {
 
     @GetMapping("/categories")
     public String getAllCategories(ModelMap model) {
+        model.addAttribute("page", "categories");
         List<NewsCategory> categories = this.categoryService.findAll();
         model.addAttribute("categories", categories);
         return "admin_dashboard/admin_categories";
@@ -93,6 +95,8 @@ public class AdminDashboardController {
 
     @GetMapping("/news")
     public String adminNews(ModelMap model){
+        model.addAttribute("page", "news");
+
         try {
             NewsHomeDTO mainFeatured = this.newsService.mainFeatured();
             model.addAttribute("mainFeatured", mainFeatured);
@@ -138,6 +142,7 @@ public class AdminDashboardController {
                                @RequestParam(required = false) String newsCategory,
                                @RequestParam(required = false) Integer page_number,
                                ModelMap model) {
+        model.addAttribute("page", "news");
 
         System.out.println(page_number);
         if (page_number == null) {
